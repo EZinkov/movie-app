77f93c5e96d88cb487e041cbefe9b3a5f9e83c2c
EZinkov <evgeniyzinkov98@gmail.com>
2023-02-21 19:46:30 +0100

    Add hero page

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,15 @@
-import React from "react"
+import React, { useEffect } from "react"
 
-const HeroSection = () => {
+const HeroSection = ({ movies, setMovies }) => {
+  useEffect(() => {
+    const API_KEY = process.env.REACT_APP_TMDB_API_KEY
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+    )
+      .then(res => res.json())
+      .then(data => setMovies(data.results))
+  }, [])
+
   return <div></div>
 }
 
