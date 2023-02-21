83ee1d342a840d06c45397d8616bfc1c2f1b5774
EZinkov <evgeniyzinkov98@gmail.com>
2023-02-21 19:49:36 +0100

    Add watchlist

--- src/pages/Watchlist.js
+++ src/pages/Watchlist.js
@@ -1,22 +1,9 @@
 import React, { useState } from "react"
-import { Route } from "react-router-dom"
-import MovieList from "../components/MovieList"
 
 const Watchlist = () => {
-  const [watchlist, setWatchlist] = useState([])
-
-  const addToWatchlist = movie => {
-    setWatchlist([...watchlist, movie])
-  }
-
-  console.log(watchlist)
-
   return (
     <div>
       <h1>My Watchlist</h1>
-      <Route path="/watchlist">
-        <MovieList movies={watchlist} onAddToWatchlist={addToWatchlist} />
-      </Route>
     </div>
   )
 }
