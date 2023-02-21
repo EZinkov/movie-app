8cfccf281ffe8737ec4632ef31ec415fb75f3096
EZinkov <evgeniyzinkov98@gmail.com>
2023-02-21 19:49:12 +0100

    Add home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
-import React from "react"
-
+import React, { useState } from "react"
 import SearchMovie from "../components/SearchMovie"
+import HeroSection from "../components/HeroSection"
+import Movies from "../components/Movies"
 
 const Home = () => {
+  const [movies, setMovies] = useState([])
   return (
     <div>
-      <SearchMovie />
+      <HeroSection movies={movies} setMovies={setMovies} />
+      <SearchMovie movies={movies} setMovies={setMovies} />
+      <Movies movies={movies} setMovies={setMovies} />
     </div>
   )
 }
