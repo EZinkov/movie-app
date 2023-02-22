6c7dafcb58b059173c94a545eb3aef4cd6505b5d
EZinkov <evgeniyzinkov98@gmail.com>
2023-02-22 19:27:39 +0100

    Add context

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-
+import { MoviesContext } from "./context/MoviesContext"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import MovieDetail from "./pages/MovieDetail"
 import Watchlist from "./pages/Watchlist"
+import { GlobalProvider } from "./context/GlobalState"
 
 const App = () => {
+  const [movies, setMovies] = React.useState([])
   return (
-    <div>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/watchlist" element={<Watchlist />} />
-          <Route path="/movie/:id" element={<MovieDetail />} />
-        </Routes>
-      </Router>
+    <div className="bg-slate-800 text-white">
+      <MoviesContext.Provider value={{ movies, setMovies }}>
+        <GlobalProvider>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/watchlist" element={<Watchlist />} />
+              <Route path="/movie/:id" element={<MovieDetail />} />
+            </Routes>
+          </Router>
+        </GlobalProvider>
+      </MoviesContext.Provider>
     </div>
   )
 }
