780b2f3e34a4fa1d07ff885ab4e1c677422666c1
EZinkov <evgeniyzinkov98@gmail.com>
2023-02-22 19:28:12 +0100

    Update hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,16 +1,34 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useContext, useState } from "react"
+import { MoviesContext } from "../context/MoviesContext"
+import ReactPaginate from "react-paginate"
 
-const HeroSection = ({ movies, setMovies }) => {
+const HeroSection = () => {
+  const [currentPage, setCurrentPage] = useState(0)
+
+  const { setMovies } = useContext(MoviesContext)
   useEffect(() => {
     const API_KEY = process.env.REACT_APP_TMDB_API_KEY
     fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${
+        currentPage + 1
+      }`
     )
       .then(res => res.json())
       .then(data => setMovies(data.results))
-  }, [])
+  }, [setMovies, currentPage])
 
-  return <div></div>
+  return (
+    <div>
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={10} // Change this to the total number of pages
+        onPageChange={({ selected }) => setCurrentPage(selected)}
+        containerClassName={"pagination"}
+        activeClassName={"active"}
+      />
+    </div>
+  )
 }
 
 export default HeroSection
