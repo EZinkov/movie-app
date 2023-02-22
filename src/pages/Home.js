14bae3ec16ec1e06efa2c39b515dd437b57f2727
EZinkov <evgeniyzinkov98@gmail.com>
2023-02-22 19:30:01 +0100

    Update homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,52 @@
-import React, { useState } from "react"
+import React, { useState, useContext, useEffect } from "react"
 import SearchMovie from "../components/SearchMovie"
-import HeroSection from "../components/HeroSection"
 import Movies from "../components/Movies"
+import { MoviesContext } from "../context/MoviesContext"
+import ReactPaginate from "react-paginate"
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs"
 
 const Home = () => {
-  const [movies, setMovies] = useState([])
+  const { setMovies } = useContext(MoviesContext)
+  const [currentPage, setCurrentPage] = useState(0)
+
+  useEffect(() => {
+    const API_KEY = process.env.REACT_APP_TMDB_API_KEY
+
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${
+        currentPage + 1
+      }`
+    )
+      .then(res => res.json())
+      .then(data => setMovies(data.results))
+  }, [setMovies, currentPage])
+
   return (
     <div>
-      <HeroSection movies={movies} setMovies={setMovies} />
-      <SearchMovie movies={movies} setMovies={setMovies} />
-      <Movies movies={movies} setMovies={setMovies} />
+      <SearchMovie />
+      <div>
+        <h1 className="text-center mt-6 font-bold">Popular movies now</h1>
+        <Movies />
+        <ReactPaginate
+          previousLabel={
+            <span className="w-10 h-10 flex mr-4 items-center justify-center bg-gray-300 rounded-md">
+              <BsChevronLeft />
+            </span>
+          }
+          nextLabel={
+            <span className="w-10 h-10 flex ml-4 items-center justify-center bg-gray-300 rounded-md">
+              <BsChevronRight />
+            </span>
+          }
+          pageCount={10}
+          onPageChange={({ selected }) => setCurrentPage(selected)}
+          containerClassName="flex items-center justify-center mt-8 mb-4"
+          activeClassName="bg-slate-500"
+          pageClassName="block border border-solid border-gray-300 hover:bg-gray-400 w-10 h-10 flex items-center justify-center rounded-md mr-4"
+          breakLabel=""
+          pageRangeDisplayed={10}
+        />
+      </div>
     </div>
   )
 }
